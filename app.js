d7f9cdd4ba1e751e40c068f5ed8400aef9551f1f
Abdel QERR <qerrouaniabdelkader@gmail.com>
2022-10-08 15:25:09 +0200

    add server express

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("nhello"); 
+const express = require('express');
+
+const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send('nhello')
+})
+
+app.listen(4000, () => {
+    console.log('the server is listening on 3000 port ')
+})
